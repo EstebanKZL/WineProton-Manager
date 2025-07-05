387d8c01893d1631cd8c08a79d5649910f0175c3
Esteban <89585806+EstebanKZL@users.noreply.github.com>
2025-07-05 03:16:31 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+setup(
+    name="WineProtonManager",
+    version="1.1.0",
+    description="Gestor de entornos Wine/Proton para Linux con interfaz Qt",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="EstebanKZL",
+    author_email="tu@email.com",
+    url="https://github.com/EstebanKZL/WineProtonManager",
+    packages=find_packages(),
+    install_requires=[
+        'PyQt5>=5.15.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'wineprotonmanager=src.wineprotonmanager:main',
+        ],
+    },
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Desktop Environment :: K Desktop Environment (KDE)',
+        'Topic :: System :: Systems Administration',
+    ],
+)
